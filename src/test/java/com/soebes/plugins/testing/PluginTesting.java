172073a3849622b114838f52d5fc960adcd1562a
Karl Heinz Marbaise <kama@soebes.de>
2022-09-22 16:43:33 +0200

    Improved.

--- src/test/java/com/soebes/plugins/testing/PluginTesting.java
+++ src/test/java/com/soebes/plugins/testing/PluginTesting.java
@@ -1,14 +1,18 @@
 package com.soebes.plugins.testing;
 
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenProjectSources;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@MavenJupiterExtension(resourcesIts = false)
+import static com.soebes.itf.jupiter.extension.MavenProjectSources.ResourceUsage.NONE;
+
+@MavenJupiterExtension
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@MavenProjectSources(resourcesUsage = NONE)
 public @interface PluginTesting {
 }
